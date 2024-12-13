ac67433e3f128874551b4a68692e9d494862a63e
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-12-13 01:04:22 -0300

    first Commit

---
+++ src/main/java/com/mballem/curso/security/domain/Perfil.java
@@ -0,0 +1,32 @@
+package com.mballem.curso.security.domain;
+
+//import javax.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "perfis")
+public class Perfil extends AbstractEntity {
+	
+	@Column(name = "descricao", nullable = false, unique = true)
+	private String desc;
+	
+	public Perfil() {
+		super();
+	}
+
+	public Perfil(Long id) {
+		super.setId(id);
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+}
