66765c2bb7f799c3a741e0f8b4b28f73a016da51
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-12-26 01:25:12 -0300

    Realizando a pesquisa com ordenação da listagem de especialidades.

--- src/main/java/com/mballem/curso/security/web/controller/EspecialidadeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/EspecialidadeController.java
@@ -2,7 +2,9 @@ package com.mballem.curso.security.web.controller;
 
 import com.mballem.curso.security.domain.Especialidade;
 import com.mballem.curso.security.service.EspecialidadeService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,5 +31,10 @@ public class EspecialidadeController {
     }
 
 
+    @GetMapping({"/datatables/server"})
+    public ResponseEntity<?> getEspecilidade(HttpServletRequest resquet){
+
+        return ResponseEntity.ok(service.buscarEspecialidades(resquet));
+    }
 
 }
