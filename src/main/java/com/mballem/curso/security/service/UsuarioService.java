83883f5f48bd97e5554bc90f62aedcbd76ffba69
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-01-02 01:33:21 -0300

    Aplicando o cadastro de novos usuários e listagem

--- src/main/java/com/mballem/curso/security/service/UsuarioService.java
+++ src/main/java/com/mballem/curso/security/service/UsuarioService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,5 +61,13 @@ public class UsuarioService implements UserDetailsService {
             ? repository.findAll(datatables.getPageable())
             : repository.findByEmailOrPerfil(datatables.getSearch(), datatables.getPageable());
         return datatables.getResponse(page);
+    }
+
+    @Transactional(readOnly = false)
+    public void salvarUsuario(Usuario usuario) {
+        String crypt = new BCryptPasswordEncoder().encode(usuario.getSenha());
+        usuario.setSenha(crypt);
+
+        repository.save(usuario);
     }
 }
--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.mballem.curso.security.web.controller;
 
+import com.mballem.curso.security.domain.Perfil;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.UsuarioService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,7 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("u")
@@ -33,6 +39,22 @@ public class UsuarioController {
     public ResponseEntity<?> listarUsuariosDatatables(HttpServletRequest request){
 
         return ResponseEntity.ok(service.buscarTodos(request));
+    }
+
+    // salvar cadastro de usuario por administrador
+    @PostMapping("/cadastro/salvar")
+    public String SalvarUsuarios(Usuario usuario, RedirectAttributes attr){
+        List<Perfil> perfis = usuario.getPerfis();
+        if(perfis.size() > 2 ||
+                perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
+                perfis.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))){
+            attr.addFlashAttribute("falha", "Paciente não pode ser ADmin 1/ou Médico.");
+            attr.addFlashAttribute("usuario", usuario);
+        }else {
+            service.salvarUsuario(usuario);
+            attr.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
+        }
+        return "redirect:/u/novo/cadastro/usuario";
     }
 
 }
