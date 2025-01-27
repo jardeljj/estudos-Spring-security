361cc0769c6819ece06e51ce5429cd637a8f6f89
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-01-27 16:14:27 -0300

    selecionando a data e hora da consulta

---
+++ src/main/java/com/mballem/curso/security/service/AgendamentoService.java
@@ -0,0 +1,23 @@
+package com.mballem.curso.security.service;
+
+import com.mballem.curso.security.domain.Horario;
+import com.mballem.curso.security.repository.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private AgendamentoRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Horario>buscarHorariosNaoAgendadosPorMedicoIdeData(Long id, LocalDate data) {
+
+        return repository.findByMedicoIdAndDataNotHorarioAgendado(id, data);
+    }
+}
