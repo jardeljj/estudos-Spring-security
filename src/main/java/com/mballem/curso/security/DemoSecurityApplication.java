eaa1c1a6e3b9fb53f5b16dcede9662d2314efa0d
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-01-31 13:42:05 -0300

    Configurando o serviço de email via SMTP

--- src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -1,14 +1,30 @@
 package com.mballem.curso.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class DemoSecurityApplication {
+public class DemoSecurityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		//System.out.println(new BCryptPasswordEncoder().encode("123456"));
+
 		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+	@Autowired
+	JavaMailSender sender;
+
+	@Override
+	public void run(String... args) throws Exception {
+		SimpleMailMessage simple = new SimpleMailMessage();
+		simple.setTo("Seu.email@outlook.com.br");
+		simple.setText("Primeiro Teste se der bom deu!");
+		simple.setSubject("Testando isso aqui");
+		sender.send(simple);
 	}
 }
